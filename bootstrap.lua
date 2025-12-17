393baa806dc74a6740d413933b10f0b7bb53785e
RenneCZ <arturdremlacz@gmail.com>
2025-12-17 02:59:32 +0100

    bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RenneCZ/noxylon_script/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RenneCZ/noxylon_script/main/loader.lua"))()()
